7f0fa0fbcbc2fbee565b978afd16c901f41bdd33
Dennis Christenson <denchris32@gmail.com>
2024-11-07 12:16:34 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import sys
+import csv
+def main():
+    sort_file = open("sortorder.csv","r")
+
+    sort_sku = csv.reader(sort_file, delimiter="\n")
+    for sku in list(sort_sku):
+        id_file = open("productids.csv", "r")
+        prod_ids = csv.reader(id_file, delimiter="\n")
+        product_sku = sku[0].split(',')[0]
+        updated_order = sku[0].split(',')[2]
+
+        # print(updated_order)
+        # print(product_sku)
+        for product in list(prod_ids):
+            product_id = product[0].split(",")[0]
+            search_sku = product[0].split(",")[1]
+            if product_sku == search_sku:
+                print(product[0].split(","))
+                obj = {}
+                obj["product_id"] = product_id
+                obj["sort_order"] = updated_order
+                output_file = open("output.txt", "a")
+                output_file.write(str(obj) + ",\n")
+
+if __name__ == "__main__":
+    main()
